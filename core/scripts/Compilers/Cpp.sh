a8b133358f3fa49c2e24126da57c98c318c0e70e
flynth <flynth.dev@gmail.com>
2023-01-18 10:19:18 -0400

    Modify: compile cpp
    1~ g++ -c file -o file.o
    2~ g++  file -o .file.bin

--- core/scripts/Compilers/Cpp.sh
+++ core/scripts/Compilers/Cpp.sh
@@ -4,6 +4,12 @@ function nameFile
 	local newName=$( echo $file | cut -d "." -f1 )
 	echo .$newName.bin
 }
+function PreName
+{
+  local file=$1
+	local newName=$( echo $file | cut -d "." -f1 )
+	echo $newName.o
+}
 
 
 function main
@@ -11,12 +17,15 @@ function main
   local file=$1
   local path=$2
   local name=$( nameFile $file )
+  local preCompile=$( PreName $file )
   
   if [[ $file == "main.cpp" ]]
   then
     local log=$( make )
   else
-    local log=$( g++ $path/$file -o $path/$name )
+    #local log=$( g++ $path/$file -o $path/$name )
+    local log=$( g++ -c $path/$file -o $path/$preCompile )
+    local log=$( g++  $path/$file -o $path/$name )
   fi
 }
 
