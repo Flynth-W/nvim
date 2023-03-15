0b26ac31bd60d78390b526fe4fb47f38c1c64590
flynth <flynth.dev@gmail.com>
2023-03-15 17:32:56 -0400

    Installer add .intall

---
+++ .install.sh
@@ -0,0 +1,15 @@
+function main
+{
+
+	# Peacker lua
+	git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim 
+  # Plug
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+  sudo pacman -S lua textlive-most pandoc ccls deno rust-analyzer texlab pyright lua-language-server 
+  sudo npm i -g vscode-langservers-extracted vim-language-server bash-language-server
+
+  echo -e " execute  :PlugInstall \n:PackerInstall"
+}
+main
